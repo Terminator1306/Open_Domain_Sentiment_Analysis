df6532596c245639d7c3b41080360c8271c27291
YAN <8745928@163.com>
2017-04-15 00:27:04 +0800

    添加web

---
+++ db/dbconnect.py
@@ -0,0 +1,10 @@
+# coding: utf-8
+import MySQLdb
+
+
+def connect(db_name='tmall'):
+    db = MySQLdb.connect("127.0.0.1", "root", "", db_name)
+    c = db.cursor()
+    c.execute("set names utf8")
+    return c
+
--- product_sentiment_analysor/settings.py
+++ product_sentiment_analysor/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'web'
 ]
 
 MIDDLEWARE = [
@@ -75,10 +76,17 @@ WSGI_APPLICATION = 'product_sentiment_analysor.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'gp_web',
+        'USER':'root',
+        'PASSWORD':'',
     }
+
 }
 
 
